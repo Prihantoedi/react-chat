3732edde5a44dae8eb55009601382cdc66f80f30
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-03-25 16:58:10 +0700

    add the message page

---
+++ src/pages/chat/index.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+import styles from './styles.module.css';
+// import Message
+
+
+const Chat = ({socket}) => {
+  return (
+    <div className={styles.chatContainer}>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Chat;
